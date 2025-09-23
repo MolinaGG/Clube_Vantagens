b26141a9d2f74bc7a4c4408472cb618d594ba28a
Guilherme Molina Solano <83838706+MolinaGG@users.noreply.github.com>
2025-09-22 23:03:59 -0300

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1f20fa3482414892"; }
+/******/ 		__webpack_require__.h = function() { return "5213ae99cb9e5b13"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
